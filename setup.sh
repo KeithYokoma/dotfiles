5dcc130c97881b9b0f5d948f65f9f10ff5a9b27a
KeithYokoma <Keith.Yokoma@gmail.com>
2016-08-17 18:11:04 +0900

    Initial Commit

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "› installing command line tools"
+sudo xcodebuild -license
+xcode-select --install
+
+echo "› downloading homebrew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "› brew update"
+brew update
+echo "› brew install ansible"
+brew install ansible
+echo "› brew install git"
+brew install git
+
+echo "› set application directory for brew cask"
+echo 'export HOMEBREW_CASK_OPTS="--appdir=/Applications"' >> ~/.bash_profile
+source ~/.bash_profile
+
+echo "› set up ansible provisioning"
+mkdir ~/.provisioning && cd $_
+git clone https://github.com/KeithYokoma/dotfiles.git
+ansible-playbook -i hosts -vv provisioning.yml
